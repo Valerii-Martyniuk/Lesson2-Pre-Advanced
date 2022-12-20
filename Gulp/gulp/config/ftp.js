65b4649c5ef65b11919d4aef9255a65376af8a95
Valerii-Martyniuk <114944293+Valerii-Martyniuk@users.noreply.github.com>
2022-12-20 21:33:17 +0200

    Add files via upload

---
+++ Gulp/gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host:"fe80::97eb:c6d9:4e6:3b1b%8",
+    user:"Valerii",
+    password:"",
+    parallel: 5,
+}
---
+++ Gulp/src/js/app.js
@@ -0,0 +1,7 @@
+console.log('Hello!');
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
